8bafc5964b02d68c78e375586cb4ff0fecb693d6
Sophia <swestwood92@gmail.com>
2013-04-11 21:40:17 -0700

    Removed unneeded code in adapter.js

--- real_server/adapter.js
+++ real_server/adapter.js
@@ -1,3 +1,5 @@
+/* Setup code, verify Chrome. */
+
 // normalize environment
 var RTCPeerConnection = null,
   getUserMedia = null,
@@ -6,84 +8,47 @@ var RTCPeerConnection = null,
   browser = null,
   webRTCSupport = true;
 
-if (navigator.mozGetUserMedia) {
-  logger.log("This appears to be Firefox");
-
-  browser = "firefox";
-
-  // The RTCPeerConnection object.
-  RTCPeerConnection = mozRTCPeerConnection;
-
-  // The RTCSessionDescription object.
-  RTCSessionDescription = mozRTCSessionDescription;
-
-  // The RTCIceCandidate object.
-  RTCIceCandidate = mozRTCIceCandidate;
-
-  // Get UserMedia (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.mozGetUserMedia.bind(navigator);
-
-  // Attach a media stream to an element.
-  attachMediaStream = function(element, stream) {
-    element.mozSrcObject = stream;
-    element.play();
-  };
-
-  reattachMediaStream = function(to, from) {
-    to.mozSrcObject = from.mozSrcObject;
-    to.play();
-  };
-
-  // Fake get{Video,Audio}Tracks
-  MediaStream.prototype.getVideoTracks = function() {
-    return [];
-  };
-
-  MediaStream.prototype.getAudioTracks = function() {
-    return [];
-  };
-} else if (navigator.webkitGetUserMedia) {
+if (navigator.webkitGetUserMedia) {
   browser = "chrome";
 
   // The RTCPeerConnection object.
   RTCPeerConnection = webkitRTCPeerConnection;
+  /* TODO: Read and verify if any of this code is necessary */
+  // // Get UserMedia (only difference is the prefix).
+  // // Code from Adam Barth.
+  // getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
 
-  // Get UserMedia (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+  // // Attach a media stream to an element.
+  // attachMediaStream = function(element, stream) {
+  //   element.autoplay = true;
+  //   element.src = webkitURL.createObjectURL(stream);
+  // };
 
-  // Attach a media stream to an element.
-  attachMediaStream = function(element, stream) {
-    element.autoplay = true;
-    element.src = webkitURL.createObjectURL(stream);
-  };
+  // reattachMediaStream = function(to, from) {
+  //   to.src = from.src;
+  // };
 
-  reattachMediaStream = function(to, from) {
-    to.src = from.src;
-  };
+  // // The representation of tracks in a stream is changed in M26.
+  // // Unify them for earlier Chrome versions in the coexisting period.
+  // if (!webkitMediaStream.prototype.getVideoTracks) {
+  //   webkitMediaStream.prototype.getVideoTracks = function() {
+  //     return this.videoTracks;
+  //   };
+  //   webkitMediaStream.prototype.getAudioTracks = function() {
+  //     return this.audioTracks;
+  //   };
+  // }
 
-  // The representation of tracks in a stream is changed in M26.
-  // Unify them for earlier Chrome versions in the coexisting period.
-  if (!webkitMediaStream.prototype.getVideoTracks) {
-    webkitMediaStream.prototype.getVideoTracks = function() {
-      return this.videoTracks;
-    };
-    webkitMediaStream.prototype.getAudioTracks = function() {
-      return this.audioTracks;
-    };
-  }
-
-  // New syntax of getXXXStreams method in M26.
-  if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
-    webkitRTCPeerConnection.prototype.getLocalStreams = function() {
-      return this.localStreams;
-    };
-    webkitRTCPeerConnection.prototype.getRemoteStreams = function() {
-      return this.remoteStreams;
-    };
-  }
+  // // New syntax of getXXXStreams method in M26.
+  // if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
+  //   webkitRTCPeerConnection.prototype.getLocalStreams = function() {
+  //     return this.localStreams;
+  //   };
+  //   webkitRTCPeerConnection.prototype.getRemoteStreams = function() {
+  //     return this.remoteStreams;
+  //   };
+  // }
 } else {
   webRTCSupport = false;
-  throw new Error("Browser does not appear to be WebRTC-capable");
+  console.error("Browser does not appear to be (Chrome) WebRTC-capable");
 }
