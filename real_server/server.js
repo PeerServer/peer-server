2aa9a007bc99437f5282c02f0926264cbfa3185b
Sophia <swestwood92@gmail.com>
2013-04-11 21:30:53 -0700

    Documented server.js, naming

--- real_server/server.js
+++ real_server/server.js
@@ -1,15 +1,25 @@
+/* Set up bindings for real server. 
+
+  Handle handshake between clientServer and clientBrowser
+*/
+
+/* Load in dependencies */
 var config = require('getconfig'),
   uuid = require('node-uuid'),
   http = require('http'),
   fs = require('fs');
 var app = require('express')();
+
+/* Create the real server for the app */
 var server = require('http').createServer(app);
 var io = require('socket.io');
 
+/* Start the server at the port. */
 server.listen(config.server.port, function() {
   console.log('Server running at: http://localhost:' + config.server.port);
 });
 
+/* Static file mappings */
 app.get('/server', function(req, res) {
   res.sendfile(__dirname + '/server/index.html');
 });
@@ -30,57 +40,69 @@ app.get('/adapter.js', function(req, res) {
   res.sendfile(__dirname + '/adapter.js');
 });
 
+
+/* Real server is notified when a browser attaches to it. 
+     socket = a user connecting to our real server. May become */
 io = io.listen(server);
-io.sockets.on('connection', function(client) {
+io.sockets.on('connection', function(socket) {
   
-  client.on('joinAsServer', function () {
-    console.log("server joined");
-    client.join("server");
-    client.emit("setClientID", client.id);
+  /* Add the socket to the client server pool */
+  socket.on('joinAsServer', function () {
+    console.log("client server joined");
+    socket.join("clientServer");
+    socket.emit("setClientID", socket.id);  // TODO necessary?
   });
 
-  client.on('joinAsClient', function () {
-    console.log("client joined");
-    client.join("client");
-    io.sockets.in("server").emit('joined', client.id);
-    client.emit("setClientID", client.id);
-    client.emit("joinedToServer", client.id);
+  /* Add the socket to the client browser pool */
+  socket.on('joinAsClient', function () {
+    console.log("client browser joined");
+    socket.join("clientBrowser");
+    io.sockets.in("clientServer").emit('joined', socket.id);
+    socket.emit("setClientID", socket.id);
+    socket.emit("joinedToServer", socket.id);
   });
   
-  client.on("sendOffer", function(sessionDescription) {
-    io.sockets.in("server").emit("receiveOffer", client.id, sessionDescription);
+  /* Next part of handshake -- client-browser sends offer, trigger a receive offer
+    on the client-server. */
+  socket.on("sendOffer", function(sessionDescription) {
+    io.sockets.in("clientServer").emit("receiveOffer", socket.id, sessionDescription);
   });
 
-  client.on("sendAnswer", function(clientID, sessionDescription) {
+  /* Client-server acknowledges offer, trigger a receive answer on the client-browser. */
+  socket.on("sendAnswer", function(clientID, sessionDescription) {
     io.sockets.socket(clientID).emit("receiveAnswer", sessionDescription);
   });
 
-  //Receive ICE candidates and send to the correct socket
-  client.on('sendICECandidate', function(clientID, candidate) {
+  /* Receive ICE candidates and send to the correct socket. 
+  ICE = Interactive Communication Establishment (google for details) */
+  socket.on('sendICECandidate', function(clientID, candidate) {
     if (clientID == "server") {
-      io.sockets.in("server").emit("receiveICECandidate", client.id, candidate);
+      io.sockets.in("clientServer").emit("receiveICECandidate", socket.id, candidate);
     } else {
       io.sockets.socket(clientID).emit("receiveICECandidate", candidate);
     }
   });
   
-  client.on('disconnect', function () {
-    console.log("disconnect");
-    var rooms = io.sockets.manager.roomClients[client.id];
-    for (var name in rooms) {
-      console.log(name);
-      if (name == "server") {
-        console.log("server disconnected");
-        io.sockets.in("client").emit('serverDisconnected');
-      } else if (name == "client") {
-        console.log("client disconnected");
-        io.sockets.in("server").emit('clientDisconnected', client.id);
-      }
-    }
-  });
+  /* Disconnect client server and client browser 
+  TODO untested -- artifact from other code. */
+  // socket.on('disconnect', function () {
+  //   console.log("disconnect");
+  //   var rooms = io.sockets.manager.roomClients[socket.id];
+  //   for (var name in rooms) {
+  //     console.log(name);
+  //     if (name == "server") {
+  //       console.log("server disconnected");
+  //       io.sockets.in("clientBrowser").emit('serverDisconnected');
+  //     } else if (name == "client") {
+  //       console.log("client disconnected");
+  //       io.sockets.in("clientServer").emit('clientDisconnected', socket.id);
+  //     }
+  //   }
+  // });
   
 });
 
+/* Set UID of process from config if applicable */
 if (config.uid) {
   process.setuid(config.uid);
 }
