b1e5f84615def710962477f18336bd4f39ba8ff7
Sophia <swestwood92@gmail.com>
2013-05-09 17:18:03 -0700

    Modified compilation so that compiled js files go into a separate dir.
    Remove your previously compiled *.js files!

--- scripts/coffee.sh
+++ scripts/coffee.sh
@@ -1,1 +1,3 @@
-coffee -wc ../real_server/*/*.coffee ../real_server/*/*/*.coffee
+(coffee -wco ../real_server/client/compiled ../real_server/client/*.coffee &); 
+(coffee -wco ../real_server/server/js/compiled ../real_server/server/js/*.coffee &);
+(coffee -wco ../real_server/shared/compiled ../real_server/shared/*.coffee &);
